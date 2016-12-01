1c1f507b5218d8ce08888cb2a4e586e6e4e5ef7e
Colin Ashburn <cashburn@purdue.edu>
2016-12-01 14:17:43 -0500

    Replaced with .NET Core

---
+++ src/YouMixMusic/Controllers/PlaylistController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+
+namespace YouMixMusic.Controllers
+{
+    [Route("api/[controller]")]
+    public class PlaylistController : Controller
+    {
+        // GET: api/Playlist
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Playlist/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Playlist
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Playlist/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Playlist/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
