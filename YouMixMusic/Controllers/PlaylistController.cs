7df72259409cad8598bb306c0e854756272ceba7
Anthony Goeckner <agoeckne@purdue.edu>
2016-11-30 21:58:36 -0500

    Began work on API.

--- YouMixMusic/Controllers/PlaylistController.cs
+++ YouMixMusic/Controllers/PlaylistController.cs
@@ -9,31 +9,18 @@ namespace YouMixMusic.Controllers
 {
     public class PlaylistController : ApiController
     {
-        // GET: api/Playlist
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+		/*[HttpPost]
+		[ActionName("playlist-add")]
+		public void addToPlaylist([FromBody]Playlist list, [FromBody]string yid)
+		{
+			Console.WriteLine("Added video with YID {0}.");
+		}*/
 
-        // GET: api/Playlist/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST: api/Playlist
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT: api/Playlist/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE: api/Playlist/5
-        public void Delete(int id)
-        {
-        }
-    }
+		[HttpPost]
+		[ActionName("playlist-create")]
+		public void createPlaylist([FromBody]string name) //MAKE THIS RETURN A PLAYLIST OBJECT AT SOME POINT
+		{
+			Console.WriteLine("Created a new playlist with name {0}.");
+		}
+	}
 }
